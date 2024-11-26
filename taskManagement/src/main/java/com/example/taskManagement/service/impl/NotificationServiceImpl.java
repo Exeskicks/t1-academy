646c24d32ac6deb921526c1370ebea6bd25b7d66
Exeskicks <danil.yudin.2014@list.ru>
2024-11-26 12:43:57 +0700

    exception

--- taskManagement/src/main/java/com/example/taskManagement/service/impl/NotificationServiceImpl.java
+++ taskManagement/src/main/java/com/example/taskManagement/service/impl/NotificationServiceImpl.java
@@ -1,9 +1,15 @@
 package com.example.taskManagement.service.impl;
 
+import com.example.taskManagement.aspect.LogAfterReturning;
+import com.example.taskManagement.aspect.LogAfterThrowing;
+import com.example.taskManagement.aspect.LogAround;
+import com.example.taskManagement.aspect.LogBefore;
+import com.example.taskManagement.exeption.NotificationException;
 import com.example.taskManagement.service.NotificationService;
 import com.example.taskManagement.web.request.TaskRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -15,13 +21,21 @@ public class NotificationServiceImpl implements NotificationService {
 
     private final JavaMailSender mailSender;
 
-
+    @LogBefore
+    @LogAfterThrowing
+    @LogAfterReturning
+    @LogAround
     @Override
     public void sendNotification(TaskRequest taskRequest) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo("notifications@example.com");
         message.setSubject("Task Status Updated");
         message.setText("Task ID: " + taskRequest.taskId() + " New Status: " + taskRequest.status());
-        mailSender.send(message);
+
+        try {
+            mailSender.send(message);
+        } catch (MailException e) {
+            throw new NotificationException("Failed to send notification", e);
+        }
     }
 }
